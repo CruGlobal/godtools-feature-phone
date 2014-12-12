a68b4dc386b482d7ff7ce794a17125a969e2e794
Bereket Negash <negash.bereket.se@gmail.com>
2014-12-12 03:39:26 -0800

    Update MyParser.java
    no need of a sax parser we will not use the xml anymore

--- src/MyParser.java
+++ src/MyParser.java
@@ -1,12 +1,7 @@
 
 
 import java.io.InputStream;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 public class MyParser {
 
